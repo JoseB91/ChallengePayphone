ca2d6816b09fe2a119c98c26b336197a8ec6a4de
JoseB91 <jabro.pipoec@gmail.com>
2026-06-22 15:14:29 -0500

    Initial Commit

---
+++ ChallengePayphone/ChallengePayphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengePayphoneApp.swift
+//  ChallengePayphone
+//
+//  Created by José Briones on 22/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ChallengePayphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
