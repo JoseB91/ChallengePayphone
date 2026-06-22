0ed4e0d6dcfc70872b297b6f7ff7126849d6e179
JoseB91 <jabro.pipoec@gmail.com>
2026-06-22 16:39:48 -0500

    Add UserRepositoryImpl wiht loadUsers implementation, pending to add mapper to decode JSON response

---
+++ ChallengePayphone/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  ChallengePayphone
+//
+//  Created by José Briones on 22/6/26.
+//
+
+enum Constants {
+    static let usersURL = "https://jsonplaceholder.typicode.com/users" // Can be defined as environment variable
+}
--- ChallengePayphone/Data/Repositories/UserRepositoryImpl.swift
+++ ChallengePayphone/Data/Repositories/UserRepositoryImpl.swift
@@ -1,7 +1,26 @@
 //
-//  UsersRepository.swift
+//  UserRepositoryImpl.swift
 //  ChallengePayphone
 //
 //  Created by José Briones on 22/6/26.
 //
 
+import Foundation
+
+final class UserRepositoryImpl: UserRepository {
+    private let httpClient: HTTPClient
+
+    init(httpClient: HTTPClient) {
+        self.httpClient = httpClient
+    }
+    
+    func loadUsers() async throws -> [User] {
+        guard let url = URL(string: Constants.usersURL) else {
+            throw URLError(.badURL)
+        }
+        
+        let (data, response) = try await httpClient.get(from: url)
+        //let users = try UsersMapper.map(data, from: response)
+        return [User(id: 1, username: "", name: "", email: "", phone: "", city: "")]
+    }
+}
